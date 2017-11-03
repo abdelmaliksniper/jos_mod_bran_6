0955c221a469c2d1d1485da7dd5042689c8831bb
Abdelmalik Yousif Abdallah <abdelmalik19930@gmail.com>
2017-11-03 22:29:25 +0300

    Add files via upload

---
+++ product_multi_barcodes/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Multi Barcodes",
+
+    'summary': """This module allows you to assign multi barcodes on a product.
+        """,
+
+    'description': """
+        
+    """,
+
+    'author': "Abdelmalik Yousif",
+    'sequence': 1,
+    'website': "",
+    'category': 'Generic Modules',
+    'version': '1.0',
+    'currency': 'EUR',
+    'price': 18.0,
+    'depends': ['point_of_sale', 'stock'],
+
+
+    'data': [
+        'views/malik.xml',
+        'views/malik_v.xml',
+    ],
+
+    'demo': [
+        #'demo/demo.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
